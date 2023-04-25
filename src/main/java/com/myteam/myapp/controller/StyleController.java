36e7c679c766f26ec67f971c50e5ba9c653f92d2
503-5 <503-5@DESKTOP-MU3S0RG>
2023-04-25 14:14:24 +0900

    styleMain, 구매동의, 구매완료

--- src/main/java/com/myteam/myapp/controller/StyleController.java
+++ src/main/java/com/myteam/myapp/controller/StyleController.java
@@ -1,8 +1,16 @@
 package com.myteam.myapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/style")
 public class StyleController {
+	
+	@RequestMapping(value = "/styleMain.do")
+	public String styleMain() {
+		
+		return "style/styleMain";
+	}
 
 }
