a28946f72ef86bbf2ccca9fa6df55524ae13182e
503-5 <503-5@DESKTOP-MU3S0RG>
2023-04-27 14:45:11 +0900

    고객센터 공지사항, 자주묻는질문

---
+++ src/main/java/com/myteam/myapp/controller/CustomerCenterController.java
@@ -0,0 +1,45 @@
+package com.myteam.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/customerCenter")
+public class CustomerCenterController {
+	
+	@RequestMapping(value = "/customerCenterMain.do")
+	public String customerCenterMain() {
+		
+		return "customerCenter/customerCenterMain";
+	}
+	
+	@RequestMapping(value = "/customerCenterQuestion_all.do")
+	public String customerCenterQuestion_all() {
+		
+		return "customerCenter/customerCenterQuestion_all";
+	}
+	
+	@RequestMapping(value = "/customerCenterQuestion_use.do")
+	public String customerCenterQuestion_use() {
+		
+		return "customerCenter/customerCenterQuestion_use";
+	}
+	
+	@RequestMapping(value = "/customerCenterQuestion_common.do")
+	public String customerCenterQuestion_common() {
+		
+		return "customerCenter/customerCenterQuestion_common";
+	}
+	
+	@RequestMapping(value = "/customerCenterQuestion_buy.do")
+	public String customerCenterQuestion_buy() {
+		
+		return "customerCenter/customerCenterQuestion_buy";
+	}
+	
+	@RequestMapping(value = "/customerCenterQuestion_sale.do")
+	public String customerCenterQuestion_sale() {
+		
+		return "customerCenter/customerCenterQuestion_sale";
+	}
+}
