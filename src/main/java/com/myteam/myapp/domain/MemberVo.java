5f000885eb4d6c5e26824d2022da211fa7516acf
sobwo <so_bw74@naver.com>
2023-05-08 10:43:08 +0900

    member,pavement,refund,sale 테이블별 vo 생성

--- src/main/java/com/myteam/myapp/domain/MemberVo.java
+++ src/main/java/com/myteam/myapp/domain/MemberVo.java
@@ -1,5 +1,50 @@
 package com.myteam.myapp.domain;
 
 public class MemberVo {
-
+	private int memberNo;
+	private String memberName, memberPw, memberEmail, memberPhone, delYn, profileImg;
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+	public String getDelYn() {
+		return delYn;
+	}
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+	public String getProfileImg() {
+		return profileImg;
+	}
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+	
+	
 }
