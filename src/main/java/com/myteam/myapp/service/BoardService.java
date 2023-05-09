45d1e0cdc4279114a9ba9c616317f2f53b553ff3
503-5 <503-5@DESKTOP-MU3S0RG>
2023-05-09 14:22:27 +0900

    myStyle_upload db 진행중

--- src/main/java/com/myteam/myapp/service/BoardService.java
+++ src/main/java/com/myteam/myapp/service/BoardService.java
@@ -8,13 +8,7 @@ import com.myteam.myapp.domain.SearchCriteria;
 public interface BoardService {
 
 	public ArrayList<BoardVo> boardList(SearchCriteria scri);
-	
-	public int boardTotal(SearchCriteria scri);
-	
-	public int boardViewCnt(int boardNo);
-	
-	public BoardVo boardSelectOne(int boardNo);
-	
+
 	public int boardInsert(BoardVo bv);
 
 }
