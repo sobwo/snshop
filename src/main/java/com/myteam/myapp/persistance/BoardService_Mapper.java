c2aaec46fcf5fb839e7b8ed384c769e25c09c8d2
503-5 <503-5@DESKTOP-MU3S0RG>
2023-05-17 16:19:56 +0900

    like db 진행중

--- src/main/java/com/myteam/myapp/persistance/BoardService_Mapper.java
+++ src/main/java/com/myteam/myapp/persistance/BoardService_Mapper.java
@@ -3,10 +3,15 @@ package com.myteam.myapp.persistance;
 import java.util.ArrayList;
 
 import com.myteam.myapp.domain.BoardVo;
+import com.myteam.myapp.domain.LikesVo;
 
 public interface BoardService_Mapper {
 	
 	public int boardInsert(BoardVo bv);
 
 	public ArrayList<BoardVo> boardList(int memberNo);
+	
+	public int like_checkInsert(LikesVo lv);
+
+	public int boardNoTotalCnt(int boardNo);
 }
--- src/main/java/com/myteam/myapp/service/BoardService.java
+++ src/main/java/com/myteam/myapp/service/BoardService.java
@@ -4,11 +4,16 @@ package com.myteam.myapp.service;
 import java.util.ArrayList;
 
 import com.myteam.myapp.domain.BoardVo;
+import com.myteam.myapp.domain.LikesVo;
 
 public interface BoardService {
 
 	public int boardInsert(BoardVo bv);
 
 	public ArrayList<BoardVo> boardList(int memberNo);
+	
+	public int like_check(LikesVo lv);
+
+	public int boardNoTotalCnt(int boardNo);
 
 }
