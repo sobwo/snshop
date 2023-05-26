9928a3a5fd0d10a67113e694c547a7345f510400
503-5 <503-5@DESKTOP-MU3S0RG>
2023-05-26 17:07:18 +0900

    .

--- src/main/java/com/myteam/myapp/domain/OrderVo.java
+++ src/main/java/com/myteam/myapp/domain/OrderVo.java
@@ -8,34 +8,20 @@ public class OrderVo{
 	private int addressNo ;
 	private int goodsNo; 
 	private int memberNo;
-<<<<<<< HEAD
-	
-//	private TIMESTAMP orderDays;
-=======
-
 	private String orderDays;
->>>>>>> branch 'main' of https://github.com/sobwo/snshop.git
 	private String size;
 	private String pors;
 	private String orderStatus;
 	private String statusDetail;
 	private String orderstatus;
 	
-<<<<<<< HEAD
-//	public TIMESTAMP getOrderDays() {
-//		return orderDays;
-//	}
-//	public void setOrderDays(TIMESTAMP orderDays) {
-//		this.orderDays = orderDays;
-//	}
-=======
+
 	public String getOrderDays() {
 		return orderDays;
 	}
 	public void setOrderDays(String orderDays) {
 		this.orderDays = orderDays;
 	}
->>>>>>> branch 'main' of https://github.com/sobwo/snshop.git
 	public String getSize() {
 		return size;
 	}
